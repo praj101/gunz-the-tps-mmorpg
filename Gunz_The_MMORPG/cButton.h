01271d624a2e000c9087209dc310bdf9fd4a3a77
Celestialkey <Celestialkey@fbef6bb8-bedc-06fa-898d-b3a7711f07bf>
2012-04-27 03:58:08 +0000

    cButton.h header was added and completed.

---
+++ Gunz_The_MMORPG/cButton.h
@@ -0,0 +1,51 @@
+#pragma once
+
+#include "CCButton.h"
+#include "CCBmButton.h"
+
+class cButton : public CCButton{
+protected:
+	uchar			m_nIllumination;
+	DWORD			m_dwCurrentTime;
+	DWORD			m_dwLastTime;
+	DWORD			m_dwClickedTime;
+	bool			m_bClicked;
+
+	virtual void	OnMouseIn();
+	virtual void	OnMouseOut();
+	virtual void	OnButtonDown();
+	virtual void	OnButtonUp();
+	virtual void	OnButtonClick();
+
+	virtual bool	OnShow();
+	virtual void	OnHide();
+
+	virtual void	OnDraw(CCDrawContext* pDC);
+
+public:
+	cButton(const char* szName, CCWidget* pParent, CCListener* pListener);
+	virtual ~cButton();
+};
+
+class cBmButton : public CCBmButton{ 
+protected:
+	uchar			m_nIllumination;
+	DWORD			m_dwCurrentTime;
+	DWORD			m_dwLastTime;
+	DWORD			m_dwClickedTime;
+	bool			m_bClicked;
+
+	virtual void	OnMouseIn();
+	virtual void	OnMouseOut();
+	virtual void	OnButtonDown();
+	virtual void	OnButtonUp();
+	virtual void	OnButtonClick();
+
+	virtual bool	OnShow();
+	virtual void	OnHide();
+
+	virtual void	OnDraw(CCDrawContext* pDC);
+public:
+	cBmButton(const char* szName, CCWidget* pParent, CCListener* pListener);
+	virtual ~cBmButton();
+};
