9e121752da2784fe5ef315669ff9fae0239f20de
Shishka.Typ3 <Shishka.Typ3@gmail.com>
2012-04-28 22:23:09 +0000

    Translated FileInfo.h

--- Gunz_The_MMORPG/FileInfo.h
+++ Gunz_The_MMORPG/FileInfo.h
@@ -1,62 +1,59 @@
 #pragma once
-/*
-		Needs translating!
-*/
 
 #include <windows.h>
 #include <time.h>
 
-// 파일의 최근 업데이트된 시간 얻기
+//Get the file's last updated time
 BOOL GetLastUpdate(const char *pFileName, FILETIME *ft);
 
-// 파일이 업데이트 되었는가?
-//		pFileName
-//		ot			:	비교할 시간 ( 보통 오픈했을 때 시간 )
+//File been updated?
+//PFileName
+//Ot: comparison of time (usually hours when open)
 BOOL IsModifiedOutside(const char *pFileName, FILETIME ot);
 
-// Extension 제거
+//Extension Removal
 BOOL RemoveExtension(char *pRemoveExt, const char *pFileName);
 
-// Extension 교체
+//Extension Replacement
 void ReplaceExtension(char *pTargetName, const char *pSourceName, char *pExt);
 
-// 상대 경로 얻기
+//Get the relative path
 void GetRelativePath(char *pRelativePath, const char *pBasePath, const char *pPath);
 
-// 절대 경로 얻기
+//Get the absolute path
 void GetFullPath(char *pFullPath, const char *pBasePath, const char *pRelativePath);
-//또 절대 경로 얻기... current directory를 기준으로...
+//Get absolute path to another. relative to the current directory.
 void GetFullPath(char *pFullPath, const char *pRelativePath);
 
-// path 만 얻어내기..
+//Path ten thousand.
 void GetPurePath(char *pPurePath,const char *pFilename);
-// path 와 extension을 제거한 순수한 파일이름 얻기.
+//Path and file name extension, removed to obtain pure.
 void GetPureFilename(char *pPureFilename,const char *pFilename);
-// extension 을 얻어낸다
+//Extension gets the
 void GetPureExtension(char *pPureExtension,const char *pFilename);
 
-// 절대 경로인가? ( 네트워크 경로는 지원하지 않음 )
+//Is the absolute path? (The network path is not supported)
 BOOL IsFullPath(const char *pPath);
 
-// 헤더 읽기 ( 헤더의 시작 int는 헤더 전체 크기를 가지고 있다. )
+//Header read (int start of the header has a total size of the header.
 BOOL ReadHeader(HANDLE hFile, void *pHeader, int nHeaderSize);
 
-// File의 CheckSum을 구한다. (날짜, 사이즈, 파일의 내용)
+//File of the CheckSum is obtained. (Date, size, and the contents of the file)
 DWORD GetFileCheckSum(char* pszFileName);
 
-// 파일이 존재하는가?
+//File exist?
 bool IsExist(const char *filename);
 
-// 부모 디렉토리 이름을 얻는다.
+//Get the parent directory name.
 void GetParentDirectory(char* pszFileName);
 
-// 주어진 경로까지의 디렉토리를 생성한다.
+//The given path to the directory is created.
 bool MakePath(const char* pszFileName);
 
-// time_t 를 FILETIME 으로 변환 Code from MSDN
+//Time_t to convert FILETIME Code from MSDN
 void time_tToFILETIME(time_t t, LPFILETIME pft);
 
-// 파일의 WriteTime 을 설정한다
+//Set the file WriteTime
 BOOL MSetFileTime(LPCTSTR lpszPath, FILETIME ft);
 
 bool GetMyDocumentsPath(char* path);
