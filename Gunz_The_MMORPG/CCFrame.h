f8368d14470148fb804f9ceb5dd46944234b2531
Celestialkey <Celestialkey@fbef6bb8-bedc-06fa-898d-b3a7711f07bf>
2012-04-28 05:25:07 +0000

    Added CCFrame.h and added new directories.

---
+++ Gunz_The_MMORPG/CCFrame.h
@@ -0,0 +1,84 @@
+#pragma once
+
+#include "CCWidget.h"
+#include "CCLookNFeel.h"
+#include "CCBmButton.h"
+
+#define CCFRAME					"Frame"
+#define CCFRAME_CLOSE_MSG		"close"
+#define CCFRAME_MINIMIZE_MSG	"minimize"
+
+class CCFrame;
+
+struct CCFrameBtn
+{
+	bool		m_bVisible;
+	bool		m_bMouseOver;
+	bool		m_bLButtonDown;
+	sRect		m_Rect;
+	CCAnchors	m_Anchors;
+	CCFrameBtn() { 
+		m_bVisible = false; 
+		m_bLButtonDown = false; 
+		m_bMouseOver = false; 
+		m_Rect = sRect(0,0,0,0);
+	}
+	bool IsButtonDown(){ 
+		if ((m_bLButtonDown==true) && (m_bMouseOver==true)) 
+			return true; 
+		return false; 
+	}
+};
+
+class CCFrameLook{
+public:
+	virtual void OnDraw(CCFrame* pFrame, CCDrawContext* pDC);
+	virtual sRect GetClientRect(CCFrame* pFrame, sRect& r);
+};
+
+
+
+/// 움직일 수 있는 프레임으로 구성된 위젯
+class CCFrame : public CCWidget{
+	DECLARE_LOOK(CCFrameLook)
+	DECLARE_LOOK_CLIENT()
+
+protected:
+	bool	m_bDragWidget;		///< Drag Widget
+	sPoint	m_DragPoint;
+
+	bool	m_bMovable;			///< 움직일 수 있는지 여부
+	bool	m_bCanShade;		///< Shade 할 수 있는지 여부 - false이면 Shade할 수 없다.
+	bool	m_bShade;
+	sSize	m_BeforeShade;		///< Before Shade
+
+	sRect		m_OldRect;
+	CCFrameBtn	m_BtnClose;
+	CCFrameBtn	m_BtnMinimize;
+
+public:
+	bool		m_bTitleBar;	///< Use Title Bar
+
+protected:
+	virtual bool OnCommand(CCWidget* pWindow, const char* szMessage);
+	virtual bool OnEvent(CCEvent* pEvent, CCListener* pListener);
+	virtual void OnSize(int w, int h);
+	virtual bool OnShow(void);
+
+	void ResizeBtnsByAnchors(int w, int h);
+	void OnCloseButtonClick();
+	void OnMinimizeButtonClick();
+
+public:
+	CCFrame(const char* szName=NULL, CCWidget* pParent=NULL, CCListener* pListener=NULL);
+	virtual ~CCFrame(void);
+
+	void SetShade(bool bShade);
+	void SetMovable(bool bValue) { m_bMovable = bValue; }
+	CCFrameBtn*	GetCloseButton() { return &m_BtnClose; }
+	CCFrameBtn*	GetMinimizeButton() { return &m_BtnMinimize; }
+
+
+
+	virtual const char* GetClassName(void){ return CCFRAME; }
+};
