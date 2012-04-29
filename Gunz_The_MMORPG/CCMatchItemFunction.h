65651bfdbb83b7e8ac1fc1a70de6fccf5dfb87e2
Celestialkey <Celestialkey@fbef6bb8-bedc-06fa-898d-b3a7711f07bf>
2012-04-29 20:44:15 +0000

    More and more files.

---
+++ Gunz_The_MMORPG/CCMatchItemFunction.h
@@ -0,0 +1,14 @@
+#pragma once
+
+const int CalculateRentItemSellBounty( const int nBountyPrice, const int nRentHourPeriod, const int nRemainHour, bool bIsChangedToRent);
+
+
+enum CASHITEM_TYPE
+{
+	CASHITEM_GAMBLE = 0,
+	CASHITEM_SPENDABLE,
+	CASHITEM_NORMAL
+};
+
+const int CalculateCashItemToBounty(CASHITEM_TYPE nType);
+const int CalculateCashItemToBounty(CASHITEM_TYPE nType, int nResLevel, int nRentMinuteRemainder, MMatchItemSlotType slotType);
