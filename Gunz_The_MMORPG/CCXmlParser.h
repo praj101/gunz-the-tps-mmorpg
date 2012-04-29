cfbbe5d3e594687b40a917d2fce396d3b4f8f185
Celestialkey <Celestialkey@fbef6bb8-bedc-06fa-898d-b3a7711f07bf>
2012-04-29 23:21:45 +0000

    Lots and lots of files.

---
+++ Gunz_The_MMORPG/CCXmlParser.h
@@ -0,0 +1,14 @@
+#pragma once
+class MXmlElement;
+class MZFileSystem;
+
+class MXmlParser
+{
+protected:
+	virtual void ParseRoot(const char* szTagName, MXmlElement* pElement) = 0;
+public:
+	MXmlParser() {}
+	virtual ~MXmlParser() {}
+	bool ReadXml(const char* szFileName);								///< xml로부터 정보를 읽는다.
+	bool ReadXml(MZFileSystem* pFileSystem,const char* szFileName);		///< xml로부터 정보를 읽는다.
+};
