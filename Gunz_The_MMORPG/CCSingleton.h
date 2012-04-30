21e06878d2c374d9b9b0db098cbe7a9e78a7a424
Celestialkey <Celestialkey@fbef6bb8-bedc-06fa-898d-b3a7711f07bf>
2012-04-30 04:12:04 +0000

    Fixed MUtil and CCSingleton, Added some files, fix other bugs, etc... Now 3/2 my favor Shishka!

--- Gunz_The_MMORPG/CCSingleton.h
+++ Gunz_The_MMORPG/CCSingleton.h
@@ -1,11 +1,11 @@
 #include <cassert>
 
-template <typename T> class MSingleton
+template <typename T> class CCSingleton
 {
     static T* m_pSingleton;
 
 public:
-    MSingleton( void )
+    CCSingleton( void )
     {
         assert( !m_pSingleton );
 
@@ -13,7 +13,7 @@ public:
 		다중상속을 받았을때 포인터가 바르지 않은 문제를 해결한 싱클톤 템플릿
 
 		gpg 에 나온 방법
-        int offset = (int)((T*)1) - (int)(MSingleton <T>*)((T*)1);
+        int offset = (int)((T*)1) - (int)(CCSingleton <T>*)((T*)1);
         m_pSingleton = (T*)((int)this + offset);
 		*/
         
@@ -21,7 +21,7 @@ public:
         m_pSingleton = static_cast<T *>(this); 
 
 	}
-   ~MSingleton( void )
+   ~CCSingleton( void )
         {  assert( m_pSingleton );  m_pSingleton = 0;  }
     static T& GetInstance( void )
         {  assert( m_pSingleton );  return ( *m_pSingleton );  }
@@ -29,38 +29,4 @@ public:
         {  return ( m_pSingleton );  }
 };
 
-template <typename T> T* MSingleton <T>::m_pSingleton = 0;
-
-
-/*
-// 예제
-
-class TextureMgr : public MSingleton <TextureMgr>
-{
-public:
-	   char type;
-	   char type2;
-    char* GetTexture( const char* name );
-	   
-    // ...
-};
-
-char* TextureMgr::GetTexture( const char* name )
-{
-	   return NULL;
-}
-
-#define g_TextureMgr TextureMgr::GetInstance()
-
-void main()
-{
-
-
-	TextureMgr aa;
-	char * stone1 = TextureMgr::GetInstance().GetTexture( "stone1" );
-
-	//   Texture* wood6 = g_TextureMgr.GetTexture( "wood6" );
-    // ...
-}
-
-*/
+template <typename T> T* CCSingleton <T>::m_pSingleton = 0;
