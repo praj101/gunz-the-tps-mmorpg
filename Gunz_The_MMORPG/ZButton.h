5ac6ab3a2e7436963f056bddec5f802cf62d91ff
Celestialkey <Celestialkey@fbef6bb8-bedc-06fa-898d-b3a7711f07bf>
2012-05-06 02:19:19 +0000

    Fixed some header issues.

--- Gunz_The_MMORPG/ZButton.h
+++ Gunz_The_MMORPG/ZButton.h
@@ -2,30 +2,6 @@
 
 #include "CCButton.h"
 
-class ZButton : public CCButton{
-protected:
-	unsigned char	m_nIllumination;
-	DWORD			m_dwCurrentTime;
-	DWORD			m_dwLastTime;
-	DWORD			m_dwClickedTime;
-	bool			m_bClicked;
-
-	virtual void OnMouseIn(void);
-	virtual void OnMouseOut(void);
-	virtual void OnButtonDown(void);
-	virtual void OnButtonUp(void);
-	virtual void OnButtonClick(void);
-
-	virtual bool OnShow(void);
-	virtual void OnHide(void);
-
-	virtual void OnDraw(CCDrawContext* pDC);
-
-public:
-	ZButton(const char* szName, CCWidget* pParent, CCListener* pListener);
-	virtual ~ZButton(void);
-};
-
 class ZButton: public CCButton 
 {
 protected:
