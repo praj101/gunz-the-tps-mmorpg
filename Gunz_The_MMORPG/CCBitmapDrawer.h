27df45baabefc6413525d98d1b29390bf9209a1a
Shishka.Typ3 <Shishka.Typ3@gmail.com>
2012-06-03 09:12:33 +0000

    0 ERRORS!

---
+++ Gunz_The_MMORPG/CCBitmapDrawer.h
@@ -0,0 +1,27 @@
+#pragma once
+
+#include "CCTypes.h"
+
+#define FRAME_OUTLINE_WIDTH	1
+#define FRAME_WIDTH		5
+#define FRAME_INLINE_WIDTH 1
+
+class CCDrawContext;
+class CCBitmap;
+
+void HLineBitmap(CCDrawContext* pDC, int x, int y, int w, CCBitmap* pBm, bool bStretch, float fScale = 1.f);
+
+void DrawBitmapFrame2(CCDrawContext* pDC, sRect& r, sRect& cliprect,CCBitmap* Bms[9]);
+void DrawBitmapFrame9(CCDrawContext* pDC, sRect& r, CCBitmap* Bms[9],bool bStretch = true, float fScale = 1.f);
+
+void DrawBitmapFrameH3(CCDrawContext* pDC, sRect& r, CCBitmap* Bms[3]);	// Draw Horizontal 3 Bitmaps
+void DrawBitmapFrameV3(CCDrawContext* pDC, sRect& r, CCBitmap* Bms[3]);	// Draw Vertical 3 Bitmaps
+void DrawBitmapFrameCustom1(CCDrawContext* pDC, sRect& r, CCBitmap* Bms[9], bool bStretch = true );
+void DrawBitmapFrameCustom2(CCDrawContext* pDC, sRect& r, CCBitmap* Bms[9], sColor bgColor, bool bStretch = true );
+void DrawBitmapButtonCustom1( CCDrawContext* pDC, sRect& r, CCBitmap* Bms[9], bool bDown = false, bool bStretch = true );
+
+bool IsNull(CCBitmap** ps, int nCount);
+
+// Safety Call
+#define GETWIDTH(_Image)	((_Image==NULL)?0:_Image->GetWidth())
+#define GETHEIGHT(_Image)	((_Image==NULL)?0:_Image->GetHeight())
